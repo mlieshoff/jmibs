6ed4f7fb599ebda7b9a39f13ede5a369175f1eef
michaellieshoff@gmx.de <michaellieshoff@gmx.de@cbdd69d8-bfac-a62b-c799-3abdf9d2e4f4>
2010-05-06 09:50:22 +0000

    restructuring.

---
+++ trunk/jmibsparent/jmibs-api/src/main/java/org/mili/jmibs/api/BenchmarkSuiteResult.java
@@ -0,0 +1,65 @@
+/*
+ * BenchmarkSuiteResult.java
+ *
+ * 12.04.2010
+ *
+ * Copyright 2010 Michael Lieshoff
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mili.jmibs.api;
+
+import java.util.*;
+
+/**
+ * The interface defines a benchmark suite result. It provides access to the results of
+ * execution and preparation of benchmarking.
+ *
+ * @author Michael Lieshoff
+ * @version 1.1 15.04.2010
+ * @since 1.0
+ * @changed ML 23.04.2010 - extend by benchmark suite getter.
+ */
+public interface BenchmarkSuiteResult {
+
+    /**
+     * @return the underlying becnhmark suite.
+     */
+    BenchmarkSuite getBenchmarkSuite();
+
+    /**
+     * @return result created while preparation of benchmarking.
+     */
+    List<BenchmarkResult> getPrepareResults();
+
+    /**
+     * adds a benchmark result to the list of prepare results.
+     *
+     * @param br benchmark prepare result.
+     */
+    void addPrepareResult(BenchmarkResult br);
+
+    /**
+     * @return result created while execution of benchmarking.
+     */
+    List<BenchmarkResult> getExecuteResults();
+
+    /**
+     * adds a benchmark result to the list of execute results.
+     *
+     * @param br benchmark execute result.
+     */
+    void addExecuteResult(BenchmarkResult br);
+
+}
---
+++ trunk/jmibsparent/jmibs-api/src/main/java/org/mili/jmibs/api/IterationObjectLoadBenchmarkSuite.java
@@ -0,0 +1,62 @@
+/*
+ * IterationBenchmarkSuite.java
+ *
+ * 12.04.2010
+ *
+ * Copyright 2010 Michael Lieshoff
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mili.jmibs.api;
+
+import java.util.*;
+
+/**
+ * This interface defines a specially benchmark suite that is based on iteration and object
+ * loading benchmark. A benchmark suite that benchmarks 10, 1.000, and 100.000 times some
+ * benchmarks that used an object count of 10.000 and 100.000 objects should implementing this
+ * interface.
+ *
+ * @author Michael Lieshoff
+ * @version 1.0 12.04.2010
+ * @since 1.0
+ */
+public interface IterationObjectLoadBenchmarkSuite<T extends ObjectLoadBenchmark<?>> extends
+        BenchmarkSuite {
+
+    /**
+     * @return list with iterations.
+     */
+    List<Integer> getIterationList();
+
+    /**
+     * adds an iteration.
+     *
+     * @param iteration iteration to add.
+     */
+    void addIteration(int iteration);
+
+    /**
+     * @return list with count of object loading.
+     */
+    List<Integer> getObjectLoadList();
+
+    /**
+     * adds an object load.
+     *
+     * @param objectLoad objectLoad to add.
+     */
+    void addObjectLoad(int objectLoad);
+
+}
